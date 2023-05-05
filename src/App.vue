a7bcb1c8d4e853760a328acaed4ea2f6dd1b3ec7
igorpiddev <igorpiddev@gmail.com>
2023-05-05 08:29:03 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<style scoped></style>
